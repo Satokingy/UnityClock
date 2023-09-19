f91d156f6fabd6eccd6a4dbb314f21b5c71c98ca
Satokingy <sumahootakujp@gmail.com>
2023-09-20 04:20:27 +0900

    Release

---
+++ UnityClock/Assets/Scripts/DestroyText.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyText : MonoBehaviour
+{
+public GameObject targetObj;
+
+    void Start()
+    {
+        //GameObjectを破棄します。
+        Destroy(targetObj);
+    }
+}
---
+++ UnityClock/Assets/Scripts/Time.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Time : MonoBehaviour {
+
+    //テキストUIをドラッグ&ドロップ
+    [SerializeField] Text DateTimeText;
+
+    //DateTimeを使うため変数を設定
+    DateTime TodayNow;
+
+    void Start()
+    {	
+	//静止画を表示する場合はtargetFrameRateを1にしておくと節電できます
+	Application.targetFrameRate = 60;
+	Invoke(nameof(Delay), 1f);
+    }    
+
+    void Delay()
+    {
+        //時間を取得
+        TodayNow = DateTime.Now;
+
+        //テキストUIに年・月・日・秒を表示させる
+        DateTimeText.text = TodayNow.Year.ToString() + "年 " + TodayNow.Month.ToString() + "月" + TodayNow.Day.ToString() + "日 " + DateTime.Now.ToLongTimeString();
+	Invoke(nameof(Delay), 1f);
+    }
+}
